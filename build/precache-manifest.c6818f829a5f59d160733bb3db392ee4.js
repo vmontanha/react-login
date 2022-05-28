e620e4e3c798e9ccdec6f21f659870eca49dfbe8
Vinnicius MOntanha <viniciusmassis01@hotmail.com>
2022-05-28 01:37:29 -0300

    run build

---
+++ build/precache-manifest.c6818f829a5f59d160733bb3db392ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a4853d848dcd509694",
+    "url": "/static/css/main.3cdf10ec.chunk.css"
+  },
+  {
+    "revision": "e2a4853d848dcd509694",
+    "url": "/static/js/main.e2a4853d.chunk.js"
+  },
+  {
+    "revision": "dee32a4944603283d269",
+    "url": "/static/js/1.dee32a49.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7782942584a51bd187e8551790f799f3",
+    "url": "/static/media/logo.77829425.png"
+  },
+  {
+    "revision": "8386486b3725e57ffe500c10f9d59f1c",
+    "url": "/index.html"
+  }
+];
