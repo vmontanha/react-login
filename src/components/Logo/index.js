e72815af28e1b3cc8ffdd86fd4a602f3a6645fcf
Vinnicius MOntanha <viniciusmassis01@hotmail.com>
2022-05-28 01:35:40 -0300

    initial commit login exposed

---
+++ src/components/Logo/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Logo from '../../assets/Logo'
+
+export default () => <img src={Logo} />
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-/*
-* Desenvolvedor: Silvanei Martins;
-* Email: silvaneimartins_rcc@hotmail.com;
-* WhatsApp: (69) 9.8405-2620;
-* Projeto React Login App;
-*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-   document.getElementById('root')
+          <React.StrictMode>
+                    <App />
+          </React.StrictMode>,
+          document.getElementById('root')
 );
